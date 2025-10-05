b0e7c7bc98a38040369c5cedbe949056cd3ec65c
nemanja2799 <nemanjaivanovic2799@gmail.com>
2025-10-05 23:48:32 +0200

    Praticna primena za rename funkciju je promena ekstenzije fajla.

--- section 19 files sequential files and functions/rename function practical example.c
+++ section 19 files sequential files and functions/rename function practical example.c
@@ -4,11 +4,13 @@
 // rename function can also be used to move a file from one directory to another
 // by specifying the new path in the second argument of the function
 // if file alredy exists in the destination path, it will be overwritten or function will fail depending on the system
+// we can use to change file extension as well
 // 
 int main()
 {
     int result;
     // result = rename("C:\\Users\\HP\\Desktop\\c programming practice\\myFile.txt", "C:\\Users\\HP\\Desktop\\c programming practice2\\yourNewFile.txt");
+    // result = rename("C:\\Users\\HP\\Desktop\\c programming practice\\myFile.txt", "C:\\Users\\HP\\Desktop\\c programming practice2\\yourNewFile.csv");
     result = rename("myFile.txt", "yourNewFile.txt");
     if(result == 0)
     {
